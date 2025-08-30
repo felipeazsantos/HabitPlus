5e4edd461ee5848dda6db33a4c43b74e55623250
felipeazsantos <felipeasantos10@hotmail.com>
2025-08-30 12:12:26 -0300

    project created

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Felipe Azevedo Dos Santos on 30/08/25.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
